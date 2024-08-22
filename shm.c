35b8a173b15952e9c2cc159b9db19ffe67d2170e
vngpl <v.ngpl1911@gmail.com>
2024-08-22 20:13:34 +0000

    added .gitignore + shm_open case 1 (shm id exists in shm_table)

--- shm.c
+++ shm.c
@@ -7,6 +7,8 @@
 #include "proc.h"
 #include "spinlock.h"
 
+#define SHM_TABLE_SIZE 64
+
 struct {
   struct spinlock lock;
   struct shm_page {
@@ -20,7 +22,7 @@ void shminit() {
   int i;
   initlock(&(shm_table.lock), "SHM lock");
   acquire(&(shm_table.lock));
-  for (i = 0; i< 64; i++) {
+  for (i = 0; i< SHM_TABLE_SIZE; i++) {
     shm_table.shm_pages[i].id =0;
     shm_table.shm_pages[i].frame =0;
     shm_table.shm_pages[i].refcnt =0;
@@ -28,14 +30,27 @@ void shminit() {
   release(&(shm_table.lock));
 }
 
+// for uint size in mappages() use pgsize variable for default page size
+
 int shm_open(int id, char **pointer) {
+  int i;
+  struct proc *p = myproc();
 
-//you write this
+  acquire(&(shm_table.lock));
+  // case 1: shm with id exists
+  for (i = 0; i < SHM_TABLE_SIZE; i++) {
+    if (shm_table.shm_pages[i].id == (uint)id) {
+      shm_table.shm_pages[i].refcnt++;
+      mappages(p->pgdir, (void*)PGROUNDUP(p->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
+      *pointer = (char*)PGROUNDUP(p->sz);
+      p->sz += PGSIZE;
+      release(&(shm_table.lock));
+      return 0;
+    }
+  }
 
 
-
-
-return 0; //added to remove compiler warning -- you should decide what to return
+  return 0; //added to remove compiler warning -- you should decide what to return
 }
 
 
@@ -45,5 +60,5 @@ int shm_close(int id) {
 
 
 
-return 0; //added to remove compiler warning -- you should decide what to return
+  return 0; //added to remove compiler warning -- you should decide what to return
 }
